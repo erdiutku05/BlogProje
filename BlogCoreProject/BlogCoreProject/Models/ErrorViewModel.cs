41989b6a90e11454da598fcf2aacd581b2e0951a
erdiutku05 <122799001+erdiutku05@users.noreply.github.com>
2023-08-16 03:41:07 +0300

    15-08

---
+++ BlogCoreProject/BlogCoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogCoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
